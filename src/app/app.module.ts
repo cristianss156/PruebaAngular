2b58756ffb823dca836d1df3e2ef0460281b49b3
cristianss156 <cristian4silvestre@gmail.com>
2026-07-06 18:15:54 +0200

    Update to Angular 18.2 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
@@ -12,22 +12,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IvaSearchComponent } from './iva-search/iva-search.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    IvaComponent,
-    IvaDetailComponent,
-    MessagesComponent,
-    DashboardComponent,
-    IvaSearchComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        IvaComponent,
+        IvaDetailComponent,
+        MessagesComponent,
+        DashboardComponent,
+        IvaSearchComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
